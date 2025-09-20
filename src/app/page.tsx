11090aac43ee4ab6e380da35f9006f37f356c7a2
Akella Srinivas <alsrinivas.346@gmail.com>
2025-09-20 16:38:30 +0530

    Change The Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ export default function Home() {
   return (
     <div>
       hello Every one
-      <input type="text" placeholder="username"></input>
-      <input type="password" placeholder="password"></input>
-      <button>Submit</button>
+      <div className="flex flex-col max-w-30">
+        <input type="text" placeholder="please enter username"></input>
+        <input type="password" placeholder="please enter password"></input>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
