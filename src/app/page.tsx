e7233fb39399e8b94bd2c3c93d236f08bc888444
Srinu346 <alsrinivas.346@gmail.com>
2025-09-21 11:47:01 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      hello Every one
+      hello Every one Keploy Testing
       <div className="flex flex-col max-w-30">
         <input type="text" placeholder="please enter username"></input>
         <input type="password" placeholder="please enter password"></input>
